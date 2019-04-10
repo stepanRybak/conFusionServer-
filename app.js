84aafa3a58d69acfb2a4846d94f9441d7f86d31e
Stepan Rybak <stepanrbak@gmail.com>
2019-04-10 03:18:22 -0700

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -44,26 +47,24 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req, res, next) {
-  console.log(req.session);
+function auth (req, res, next) {
+  console.log(req.user);
 
-  if (!req.session.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 401;
-      return next(err);
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');                          
+    err.status = 403;
+    next(err);
   }
   else {
-    if(req.session.user === 'authenicated'){
-      next();
-    }
-    else{
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      next(err);
-    }
+        next();
   }
 }
 app.use(auth);
