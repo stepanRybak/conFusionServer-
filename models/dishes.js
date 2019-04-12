b834a9faaa0de2d35d8f894ed95c1da1b9d4ba4b
Stepan Rybak <stepanrbak@gmail.com>
2019-04-12 12:53:19 -0700

    Mongoose Population

--- models/dishes.js
+++ models/dishes.js
@@ -3,24 +3,24 @@ const Schema = mongoose.Schema;
 require('mongoose-currency').loadType(mongoose);
 var Currency = mongoose.Types.Currency;
 
-const commentSchema=new Schema({
-    rating:{
+var commentSchema = new Schema({
+    rating:  {
         type: Number,
         min: 1,
-        max:5,
+        max: 5,
         required: true
     },
-    comment:{
+    comment:  {
         type: String,
         required: true
     },
-    author:{
-        type:String,
-        required:true
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
-
-},{
-    timestamps:true
+}, {
+    usePushEach: true,
+    timestamps: true
 });
 
 var dishSchema = new Schema({
--- models/user.js
+++ models/user.js
@@ -3,10 +3,17 @@ var Schema= mongoose.Schema;
 var passwordLocalMongoose= require('passport-local-mongoose');
 
 var User = new Schema({
-    admin: {
-        type:Boolean,
-        default:false
-
+    firstname: {
+      type: String,
+        default: ''
+    },
+    lastname: {
+      type: String,
+        default: ''
+    },
+    admin:   {
+        type: Boolean,
+        default: false
     }
 });
 
