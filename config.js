ce8715d21d20732689544e8c9de1f1214ee2ef39
stepanRybak <45518681+stepanRybak@users.noreply.github.com>
2019-04-22 21:59:51 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
         clientId: '595745394235187',
-        clientSecret: '8b87251035fe4a6b2ce1da7bd01e7d74'
+        clientSecret: 'Your Client App Secret'
     }
 }
